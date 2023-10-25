de9ebcb3d68e16091bc0db482f22dfad5f9d027b
slam814 <abdussalam6111997@gmail.com>
2023-10-25 11:37:37 +0600

    I have practiced array of index

---
+++ array-of-index.js
@@ -0,0 +1,22 @@
+// Array of index 
+
+var numbers = [11, 23, 44, 66, 89, 100];
+
+// Chenge of Eliment Valu 
+
+
+console.log(numbers);
+var numbersSecendEliment = numbers[2];
+console.log(numbersSecendEliment);
+var position = numbers.indexOf(44);
+console.log(position);
+
+
+
+numbers[2] = 77;
+
+var numbersSecendEliment = numbers[2];
+console.log(numbersSecendEliment);
+
+var position = numbers.indexOf(44);
+console.log(position);
