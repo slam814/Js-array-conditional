c85e0fafc8b927a04b232acfc1f0b80c391238c9
slam814 <abdussalam6111997@gmail.com>
2023-10-25 12:06:55 +0600

    I have added push and pop this file

---
+++ arry-push-pop-retent.js
@@ -0,0 +1,13 @@
+var numbers = [10, 14, 13, 77, 88, 25];
+
+var pushNubers = numbers.push(30);
+
+console.log(numbers);
+console.log(pushNubers);
+
+var popNumbershele = numbers.pop();
+
+console.log(numbers);
+console.log(popNumbershele);
+
+
