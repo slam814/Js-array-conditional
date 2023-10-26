0b6d258f82c84e5671ed48fa7739e8090e441c43
slam814 <abdussalam6111997@gmail.com>
2023-10-26 14:27:42 +0600

    i added some file

---
+++ conditional.js
@@ -0,0 +1,31 @@
+var iPhonePrice = 99999;
+var myBadget = 50000000;
+
+
+if(iPhonePrice < myBadget){
+    console.log("ami iphone kine futani marbo")
+}
+
+
+var chickenPrice = 500;
+var myBadgetTaka = 100;
+
+if(chickenPrice < myBadgetTaka){
+    console.log("ami gosto khamu chibai chibai chibaia")
+}
+
+    if(chickenPrice > myBadgetTaka){
+        console.log("alu vorta kaw Tomar taka nai")
+    }
+
+
+
+    var newIphonePrice = 79000;
+    var newMyBadget   =  9000;
+
+    if(newIphonePrice < newMyBadget){
+        console.log("Iphone kino taratai");
+    }
+    else{
+        console.log("button phone chalaw akhon");
+    }
---
+++ mulipul-conditional.js
@@ -0,0 +1,12 @@
+
+var isGraduated = true;
+var salary  = 500;
+var car = 1;
+
+if(isGraduated == true && salary == 50000 || isGraduated == true && car == 1){
+    console.log("Eso bia kore fali ...isGraduated.........")
+
+}
+else{
+    console.log("tor kopale bia nai")
+}
